931fe419f39569aa0803f258b1d0539752bab9df
Frederik Rietdijk <fridh@fridh.nl>
2020-02-17 16:46:39 +0100

    Step towards fixing tests

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,55 +1,65 @@
-
-import seapy
-import numpy as np
 from weakref import WeakSet
 
-class TestSystem():
+from acoustics.signal import OctaveBand
+import numpy as np
+import seapy
+
+import pytest
+
+
+@pytest.fixture
+def system():
+    frequency = OctaveBand(fstart=500.0, fstop=8000.0, fraction=1)
+    system = seapy.system.System(frequency)
+    return system
+
+
+
+class TestSystem:
     """
     New system.
     """
 
-    def test_new_system(self):
+    def test_new_system(self, system):
         """
         New System.
         """
-        system1 = seapy.system.System()
+        pass
 
-    def test_frequencies(self):
-        system1 = seapy.system.System()
+    # def test_frequencies(self):
+    #     frequency = OctaveBand(fstart=500.0, fstop=8000.0, fraction=1)
+    #     system1 = seapy.system.System(frequency)
         
-        system1.frequency.center = [500.0, 1000.0, 2000.0, 4000.0, 8000.0]
-        system1.frequency.lower = [355.0, 710.0, 1420.0, 2840.0, 5680.0] 
-        system1.frequency.upper = [710.0, 1420.0, 2840.0, 5680.0, 11360.0] 
-        system1.frequency.enabled = [False, False, True, True, True]
+        # Setting is not yet implemented
+    #     system1.frequency.center = [500.0, 1000.0, 2000.0, 4000.0, 8000.0]
+    #     system1.frequency.lower = [355.0, 710.0, 1420.0, 2840.0, 5680.0] 
+    #     system1.frequency.upper = [710.0, 1420.0, 2840.0, 5680.0, 11360.0] 
+    #     system1.frequency.enabled = [False, False, True, True, True]
     
-class TestNewObject():
+class TestNewObject:
     """Test adding a new object to the System.
     """
 
-    def test_add_material(self):
+    def test_add_material(self, system):
         """
         Add material.
-        """
-        system1 = seapy.system.System()
+        """        
+        steel = system.add_material('steel', 'MaterialSolid', young=1.0e7, poisson=0.30, loss_factor=np.ones(len(system.frequency.center))*0.2)
         
-        steel = system1.addMaterial('steel', 'MaterialSolid', young=1.0e7, poisson=0.30, loss_factor=np.ones(len(system1.frequency.center))*0.2)
-        
-        assert(len(list(system1.objects))==1)
-        assert(len(list(system1.materials))==1)
+        assert(len(list(system.objects))==1)
+        assert(len(list(system.materials))==1)
         assert(len(list(steel.linked_components))==0)
 
-    def test_add_component(self):
+    def test_add_component(self, system):
         """
         Add component.
         """
+        steel = system.add_material('steel', 'MaterialSolid', young=1.0e7, poisson=0.30, loss_factor=np.ones(len(system.frequency.center))*0.2)
+        beam1 = system.add_component('beam1', 'Component1DBeam', material='steel', volume=10.0, length=2.0, cross_section=0.30)
         
-        system1 = seapy.system.System()
-        steel = system1.addMaterial('steel', 'MaterialSolid', young=1.0e7, poisson=0.30, loss_factor=np.ones(len(system1.frequency.center))*0.2)
-        beam1 = system1.addComponent('beam1', 'Component1DBeam', material='steel', volume=10.0, length=2.0, cross_section=0.30)
-        
-        assert(len(list(system1.objects))==5) # 1 material + 1 component + 3 subsystems = 5
-        assert(len(list(system1.materials))==1)
-        assert(len(list(system1.components))==1)
+        assert(len(list(system.objects))==5) # 1 material + 1 component + 3 subsystems = 5
+        assert(len(list(system.materials))==1)
+        assert(len(list(system.components))==1)
         assert(len(list(steel.linked_components))==1)
 
         #assert(steel.__dict__['linked_components'][0] == 'beam1')
@@ -59,23 +69,20 @@ class TestNewObject():
         system1.removeObject('beam1')
 
     
-    def test_add_junction(self):
+    def test_add_junction(self, system):
         """
         Add junction.
         """
-        system1 = seapy.system.System()
-        junction1 = system1.addJunction('junction1', 'Junction', shape='Point')
-        assert(len(list(system1.junctions))==1)
+        junction1 = system.add_junction('junction1', 'Junction', shape='Point')
+        assert(len(list(system.junctions))==1)
 
-    def test_add_excitation(self):
+    def test_add_excitation(self, system):
         """
         Add junction.
         """
-        system1 = seapy.system.System()
-        
-        steel = system1.addMaterial('steel', 'MaterialSolid', young=1.0e7, poisson=0.30, loss_factor=np.ones(len(system1.frequency.center))*0.2)
-        beam1 = system1.addComponent('beam1', 'Component1DBeam', material='steel', volume=10.0, length=2.0, cross_section=0.30)
+        steel = system.add_material('steel', 'MaterialSolid', young=1.0e7, poisson=0.30, loss_factor=np.ones(len(system.frequency.center))*0.2)
+        beam1 = system.add_component('beam1', 'Component1DBeam', material='steel', volume=10.0, length=2.0, cross_section=0.30)
         
         subsystem1 = beam1.subsystem_long
-        ex1 = subsystem1.addExcitation('ex1', 'ExcitationPointForce')
-        assert(len(list(system1.excitations))==1)
+        ex1 = subsystem1.add_excitation('ex1', 'ExcitationPointForce')
+        assert(len(list(system.excitations))==1)
